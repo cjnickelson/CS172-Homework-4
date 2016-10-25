ae47e42cad81b9ba3bad4c019ffd4b4c23cca1ab
Carter Nickelson <cnickelson19@my.whitworth.edu>
2016-10-25 14:40:38 -0700

    implemented UML diagram

--- CS172 Homework 4/CS172 Homework 4/Rectangle2D.h
+++ CS172 Homework 4/CS172 Homework 4/Rectangle2D.h
@@ -20,3 +20,29 @@ public:
 	const bool contains(Rectangle2D& r);
 	const bool overlaps(Rectangle2D& r);
 };
+
+/*
+               UML Diagram
+___________________________________________
+- double x
+- double y
+- double width
+- double height
+___________________________________________
++ double getX()
++ double getY()
++ double getWidth()
++ double getHeight()
++ void setX(double)
++ void setY(double)
++ void setWidth(double)
++ void setHeight(double)
++ Rectangle2D()
++ Rectangle2D(double, double, double, double)
++ const double getArea()
++ const double getPerimeter()
++ cosnt bool contains(double, double)
++ const bool contains(Rectangle2D &r)
++ const bool overlaps(Rectangle2D &r)
+___________________________________________
+*/
